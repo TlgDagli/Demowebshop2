289b0f24a7f84d0783c66e77231488319bdab69f
ay45669900 <kaparaycin@gmail.com>
2024-03-09 01:12:08 +0300

    deneme

---
+++ src/US_205.java
@@ -0,0 +1,10 @@
+import Utlity.BaseDriver;
+import org.testng.annotations.Test;
+
+public class US_205 extends BaseDriver {
+    @Test
+    public void Test(){
+
+        driver.get("https://demowebshop.tricentis.com");
+    }
+}
