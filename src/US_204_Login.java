1f35de6b27781e84b49e3a4281c651948e0caae3
Ali kemal çakıral <107760881+mavrosispira@users.noreply.github.com>
2024-03-10 19:23:33 +0300

    ders2

--- src/US_204_Login.java
+++ src/US_204_Login.java
@@ -1,10 +1,11 @@
+import Utlity.BaseDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import static Utlity.BaseDriver.driver;
 
-public class US_204_Login {
+public class US_204_Login extends BaseDriver {
     @Test
     public void test1() {
         for (int i = 0; i < 5; i++) {
