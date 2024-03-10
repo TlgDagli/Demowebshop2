a173f9d64649e8adc3c39993dd12095cead7e971
Ali kemal çakıral <107760881+mavrosispira@users.noreply.github.com>
2024-03-10 19:19:44 +0300

    ders1

--- src/US_204_Login.java
+++ src/US_204_Login.java
@@ -1,9 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import static Utlity.BaseDriver.driver;
 
 public class US_204_Login {
     @Test
     public void test1() {
-        System.out.println("MerhabaDemoMerhaba");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World");
+        }
     }
 
 }
