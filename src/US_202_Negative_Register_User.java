40c0efd20e15a5f26b4ffabbdb6dd8297d620eff
ArafatTechnoStudy <arafatozturk2019@gmail.com>
2024-03-11 23:02:53 +0300

    US-202 devam

--- src/US_202_Negative_Register_User.java
+++ src/US_202_Negative_Register_User.java
@@ -1,5 +1,51 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static Utlity.BaseDriver.driver;
+
+/*
+
+US_202_RegisterNegative:
+
+1. Web tarayıcısını açın ve "demowebshop.tricentis.com" adresine gidin.
+2. Anasayfada "Register" butonuna tıklayın.
+3. "Your Personal Details" bölümünde test datada verilen bilgileri doldurun:
+"Register" butonuna tıklayarak kayıt işlemi gerçekleştirin.
+
+Environment:
+https://demowebshop.tricentis.com
+
+Kişisel Bilgiler:
+Cinsiyet: Erkek (Male)
+Ad: Sdet3
+Soyad: Tears4
+E-posta: sdettears2@gmail.com
+Şifre: Pass1234
+Şifre Onay: Pass1234
+ */
+
 public class US_202_Negative_Register_User {
-    public static void main(String[] args) {
-        System.out.println("MerhabaDemoMerhaba");
+    @Test
+    public void NegativeRegister(){
+        driver.get("https://demowebshop.tricentis.com/");
+        WebElement Register= driver.findElement(By.xpath("//a[text()='Register']"));
+        Register.click();
+
+        WebElement male= driver.findElement(By.xpath("//*[@id='gender-male']"));
+        male.click();
+
+        WebElement firstName= driver.findElement(By.xpath("//*[@id='FirstName']"));
+        firstName.click();
+
+        WebElement lastName= driver.findElement(By.xpath("//*[@id='LastName']"));
+        lastName.click();
+
+
+
+
+
+
+
     }
 }
