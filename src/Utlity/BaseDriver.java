53e6c588d00a223449c2104648db5a8526d97f1d
tdagl <tolgamoon@gmail.com>
2024-03-12 00:20:53 +0300

    Aksiyonpast

--- src/Utlity/BaseDriver.java
+++ src/Utlity/BaseDriver.java
@@ -12,7 +12,7 @@ public class BaseDriver {
 
     static { //bunun şartı extends olması ve başta yer alması
         driver = new ChromeDriver();
-        //driver.manage().window().maximize();//Ekranı max yapıyor
+        driver.manage().window().maximize();//Ekranı max yapıyor
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // 20 sn mühlet: elementi bulma mühleti
         wait=new WebDriverWait(driver, Duration.ofSeconds(20));
