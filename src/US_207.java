744375b852a46a3b6670676ce87f26c527e75f33
tdagl <tolgamoon@gmail.com>
2024-03-15 20:28:22 +0300

    207action

--- src/US_207.java
+++ src/US_207.java
@@ -1,19 +1,21 @@
 import Utlity.BaseDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class US_207 extends BaseDriver {
     @Test
     public void us_207(){
-
+        Actions aksiyonlar = new Actions(driver);
 
         WebElement community = driver.findElement(By.xpath("//input[@id='pollanswers-1']"));
-        community.click();
+        aksiyonlar.moveToElement(community).click().build().perform();
+
 
         WebElement button = driver.findElement(By.xpath("//input[@id='vote-poll-1']"));
-        button.click();
+        aksiyonlar.moveToElement(button).click().build().perform();
 
         WebElement msj = driver.findElement(By.xpath("//div[@id='block-poll-vote-error-1']"));
 
